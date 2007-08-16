khonkhortisan/bzflag-import-3

commit b74c5b5cce483d18110e89274f9f230975799a72

Convert MsgAllow to use a bitmask instead of two boolean values. Separate the allows into shoot, jump, turn left, turn right, move forward, and move backward. Bump protocol to 0052
git-svn-id: https://svn.code.sf.net/p/bzflag/code/trunk@14926 08b3d480-bf2c-0410-a26f-811ee3361c24

src/bzflag/playing.cxx
@@ -2019,11 +2019,8 @@ static void handleAllow(void *msg)
   LocalPlayer *localtank = NULL;
   msg = nboUnpackUByte(msg, id);
 
-  uint8_t allowMovement;
-  nboUnpackUByte(msg, allowMovement);
-
-  uint8_t allowShooting;
-  nboUnpackUByte(msg, allowShooting);
+  uint8_t allow;
+  nboUnpackUByte(msg, allow);
 
   Player* tank = NULL;
   if (myTank && myTank->getId() == id) {
@@ -2054,10 +2051,9 @@ static void handleAllow(void *msg)
 	}
   }
 
-  tank->setAllowMovement(allowMovement != 0);
-  tank->setAllowShooting(allowShooting != 0);
-  addMessage(tank, allowMovement ? "Movement allowed" : "Movement forbidden");
-  addMessage(tank, allowShooting ? "Shooting allowed" : "Shooting forbidden");
+  tank->setAllow(allow);
+  //addMessage(tank, allowMovement ? "Movement allowed" : "Movement forbidden");
+  //addMessage(tank, allowShooting ? "Shooting allowed" : "Shooting forbidden");
 }
 
 static void handleKilledMessage(void *msg, bool human, bool &checkScores)

include/PlayerInfo.h
@@ -76,9 +76,14 @@ public:
   bool	isPaused() const;
   bool	isAutoPilot() const;
   bool  canMove() const;
+  bool  canJump() const;
+  bool  canTurnLeft() const;
+  bool  canTurnRight() const;
+  bool  canMoveForward() const;
+  bool  canMoveBackward() const;
   bool  canShoot() const;
-  void  setAllowMovement(bool allow);
-  void  setAllowShooting(bool allow);
+  void  setAllow(unsigned char _allow);
+  unsigned char getAllow();
   bool	isBot() const;
   bool	isHuman() const;
   bool  isChat() const;
@@ -176,8 +181,7 @@ private:
 
   TimeKeeper lastFlagDropTime;
 
-  bool allowMovement;
-  bool allowShooting;
+  unsigned char allow;
 
   // spam prevention
   std::string lastMsgSent;
@@ -262,24 +266,48 @@ inline bool PlayerInfo::isPaused() const {
 
 inline bool		PlayerInfo::canMove() const
 {
-  return allowMovement;
+  return (canTurnLeft() && canTurnRight() && canMoveForward() && canMoveBackward());
+}
+
+inline bool		PlayerInfo::canJump() const
+{
+  return allow & AllowJump;
+}
+
+inline bool		PlayerInfo::canTurnLeft() const
+{
+  return allow & AllowTurnLeft;
+}
+
+inline bool		PlayerInfo::canTurnRight() const
+{
+  return allow & AllowTurnRight;
+}
+
+inline bool		PlayerInfo::canMoveForward() const
+{
+  return allow & AllowMoveForward;
+}
+
+inline bool		PlayerInfo::canMoveBackward() const
+{
+  return allow & AllowMoveBackward;
 }
 
 inline bool		PlayerInfo::canShoot() const
 {
-  return allowShooting;
+  return allow & AllowShoot;
 }
 
-inline void		PlayerInfo::setAllowMovement(bool allow)
+inline void		PlayerInfo::setAllow(unsigned char _allow)
 {
-  allowMovement = allow;
+  allow = _allow;
   allowChangeTime = now;
 }
 
-inline void		PlayerInfo::setAllowShooting(bool allow)
+inline unsigned char		PlayerInfo::getAllow()
 {
-  allowShooting = allow;
-  allowChangeTime = now;
+  return allow;
 }
 
 inline bool PlayerInfo::isAutoPilot() const {

include/global.h
@@ -59,6 +59,18 @@ enum TeamColor {
   HunterTeam = 7
 };
 
+// Player allow attributes for MsgAllow
+enum PlayerAllow {
+  AllowNone = 0x00,
+  AllowShoot = 0x01,
+  AllowJump = 0x02,
+  AllowTurnLeft = 0x04,
+  AllowTurnRight = 0x08,
+  AllowMoveForward = 0x10,
+  AllowMoveBackward = 0x20,
+  AllowAll = 0xFF
+};
+
 #ifdef ROBOT
 // robots
 #define MAX_ROBOTS 100

src/bzflag/LocalPlayer.cxx
@@ -1116,7 +1116,9 @@ void			LocalPlayer::setDesiredSpeed(float fracOfMaxSpeed)
   FlagType* flag = getFlag();
 
   // If we aren't allowed to move, then the desired speed is 0.
-  if (!canMove()) {
+  if (
+	  (!canMoveForward() && fracOfMaxSpeed > 0) ||
+	  (!canMoveBackward() && fracOfMaxSpeed < 0)) {
     fracOfMaxSpeed = 0.0;
   }
 
@@ -1175,19 +1177,14 @@ void			LocalPlayer::setDesiredAngVel(float fracOfMaxAngVel)
 {
   FlagType* flag = getFlag();
 
-  // If we aren't allowed to move, then the desired angular velocity is 0.
-  if (!canMove()) {
-    fracOfMaxAngVel = 0.0;
-  }
-
   // limit turn speed to maximum
   if (fracOfMaxAngVel > 1.0f) fracOfMaxAngVel = 1.0f;
   else if (fracOfMaxAngVel < -1.0f) fracOfMaxAngVel = -1.0f;
 
-  // further limit turn speed for certain flags
-  if (fracOfMaxAngVel < 0.0f && getFlag() == Flags::LeftTurnOnly)
+  // further limit turn speed for certain flags or when we aren't allowed to turn
+  if (fracOfMaxAngVel < 0.0f && (getFlag() == Flags::LeftTurnOnly || !canTurnLeft()))
     fracOfMaxAngVel = 0.0f;
-  else if (fracOfMaxAngVel > 0.0f && getFlag() == Flags::RightTurnOnly)
+  else if (fracOfMaxAngVel > 0.0f && (getFlag() == Flags::RightTurnOnly || !canTurnRight()))
     fracOfMaxAngVel = 0.0f;
 
   // boost turn speed for other flags
@@ -1381,6 +1378,11 @@ void			LocalPlayer::doJump()
   FlagType* flag = getFlag();
   World *world = World::getWorld();
   if (!world) {
+    return;
+  }
+
+  // Are we allowed to jump?
+  if (!canJump()) {
     return;
   }
 

src/bzflag/Player.h
@@ -126,6 +126,11 @@ public:
   bool		isPhantomZoned() const;
   bool		isCrossingWall() const;
   bool		canMove() const;
+  bool		canJump() const;
+  bool		canTurnLeft() const;
+  bool		canTurnRight() const;
+  bool		canMoveForward() const;
+  bool		canMoveBackward() const;
   bool		canShoot() const;
   bool		isNotResponding() const;
   void		resetNotResponding();
@@ -168,8 +173,8 @@ public:
   void	  setHandicap(float handicap);
   void		setStatus(short);
   void		setExplode(const TimeKeeper&);
-  void		setAllowMovement(bool allow);
-  void		setAllowShooting(bool allow);
+  void		setAllow(unsigned char _allow);
+  unsigned char		getAllow();
   void		setTeleport(const TimeKeeper&, short from, short to);
   void		endShot(int index, bool isHit = false,
 			bool showExplosion = false);
@@ -275,8 +280,7 @@ private:
   short			wins;			// number of kills
   short			losses;			// number of deaths
   short			tks;			// number of teamkills
-  bool			allowMovement;		// frozen motion
-  bool			allowShooting;		// frozen shooting
+  unsigned char			allow;		// tank allowed actions
 
   // score of local player against this player
   short			localWins;		// local player won this many
@@ -540,13 +544,38 @@ inline bool		Player::isCrossingWall() const
 inline bool		Player::canMove() const
 {
   //return (state.status & short(PlayerState::AllowMovement)) != 0;
-  return allowMovement;
+  return (canTurnLeft() && canTurnRight() && canMoveForward() && canMoveBackward());
+}
+
+inline bool		Player::canJump() const
+{
+  return allow & AllowJump;
+}
+
+inline bool		Player::canTurnLeft() const
+{
+  return allow & AllowTurnLeft;
+}
+
+inline bool		Player::canTurnRight() const
+{
+  return allow & AllowTurnRight;
+}
+
+inline bool		Player::canMoveForward() const
+{
+  return allow & AllowMoveForward;
+}
+
+inline bool		Player::canMoveBackward() const
+{
+  return allow & AllowMoveBackward;
 }
 
 inline bool		Player::canShoot() const
 {
   //return (state.status & short(PlayerState::AllowShooting)) != 0;
-  return allowShooting && getShotType() != NoShot;
+  return (allow & AllowShoot) && getShotType() != NoShot;
 }
 
 inline bool		Player::isNotResponding() const
@@ -619,14 +648,14 @@ inline void		Player::setPlayerList(bool _playerList)
   playerList = _playerList;
 }
 
-inline void		Player::setAllowMovement(bool allow)
+inline void		Player::setAllow(unsigned char _allow)
 {
-  allowMovement = allow;
+  allow = _allow;
 }
 
-inline void		Player::setAllowShooting(bool allow)
+inline unsigned char	Player::getAllow()
 {
-  allowShooting = allow;
+  return allow;
 }
 
 inline void*		Player::pack(void* buf, uint16_t& code)

src/bzfs/AutoAllowTimer.cxx
@@ -50,9 +50,8 @@ void AutoAllowTimerTickHandler::process(bz_EventData *eventData)
 
     if (!player->player.canShoot() || !player->player.canMove()) {
       if (now - player->player.allowChangeTime > autoallowtime) {
-	sendMessageAllow(player->getIndex(), true, true);
-	player->player.setAllowShooting(true);
-	player->player.setAllowMovement(true);
+	sendMessageAllow(player->getIndex(), AllowAll);
+	player->player.setAllow(AllowAll);
       }
     }
   }

src/bzfs/GameKeeper.cxx
@@ -586,7 +586,10 @@ float GameKeeper::Player::getRealSpeed ( float input )
 	float fracOfMaxSpeed = input;
 
 	// If we aren't allowed to move, then the real speed is 0.
-	if (!player.canMove())
+	if (
+		(!player.canMoveForward() && fracOfMaxSpeed > 0) ||
+		(!player.canMoveBackward() && fracOfMaxSpeed < 0)
+	)
 		return 0.0f;
 
 	// can't go faster forward than at top speed, and backward at half speed

src/bzfs/bzfs.cxx
@@ -2337,9 +2337,8 @@ void playerAlive(int playerIndex)
   sendMessageAlive(playerIndex,playerData->currentPos,playerData->currentRot);
 
   playerData->efectiveShotType = StandardShot;
-  playerData->player.setAllowMovement(true);
-  playerData->player.setAllowShooting(true);
-  sendMessageAllow(playerIndex, true, true);
+  playerData->player.setAllow(AllowAll);
+  sendMessageAllow(playerIndex, AllowAll);
 
   // call any events for a playerspawn
   bz_PlayerSpawnEventData_V1	spawnEvent;
@@ -2483,9 +2482,8 @@ void processFreezeTagCollision ( GameKeeper::Player *player, GameKeeper::Player 
   if (playerTeam == otherTeam) {
     // unfreeze
     if (!player->player.canShoot() || !player->player.canMove()) {
-      sendMessageAllow(player->getIndex(), true, true);
-      player->player.setAllowShooting(true);
-      player->player.setAllowMovement(true);
+      sendMessageAllow(player->getIndex(), AllowAll);
+      player->player.setAllow(AllowAll);
     }
   } else {
     float dx, dy, dist, angle, cos_angle;
@@ -2509,9 +2507,8 @@ void processFreezeTagCollision ( GameKeeper::Player *player, GameKeeper::Player 
     float otherDist = dist * cos_angle;
 
     if ( (playerDist - otherDist > 2.0 * BZDBCache::dmzWidth) && (player->player.canShoot() || player->player.canMove()) ) {
-      sendMessageAllow(player->getIndex(), false, false);
-      player->player.setAllowMovement(false);
-      player->player.setAllowShooting(false);
+      sendMessageAllow(player->getIndex(), AllowNone);
+      player->player.setAllow(AllowNone);
     }
   }
 }

commit 41996d4634a4f0fc826c90a9f680374f5f342a55

Fixed compilation of bzrobots (broken in r14926). Re-added statusmessages about forbidden / allowed movement. (removed in r14926)
git-svn-id: https://svn.code.sf.net/p/bzflag/code/trunk@14947 08b3d480-bf2c-0410-a26f-811ee3361c24

src/bzflag/playing.cxx
@@ -2058,8 +2058,9 @@ static void handleAllow(void *msg)
   }
 
   tank->setAllow(allow);
-  //addMessage(tank, allowMovement ? "Movement allowed" : "Movement forbidden");
-  //addMessage(tank, allowShooting ? "Shooting allowed" : "Shooting forbidden");
+  addMessage(tank, allow & AllowShoot ? "Shooting allowed" : "Shooting forbidden");
+  addMessage(tank, allow & (AllowMoveForward | AllowMoveBackward | AllowTurnLeft | AllowTurnRight) ? "Movement allowed" : "Movement restricted");
+  addMessage(tank, allow & AllowJump ? "Jumping allowed" : "Jumping forbidden");
 }
 
 static void handleKilledMessage(void *msg, bool human, bool &checkScores)

src/bzrobots/botplaying.cxx
@@ -1086,11 +1086,8 @@ static void handleAllow ( void *msg, uint16_t /*len*/ )
   LocalPlayer *localtank = NULL;
   msg = nboUnpackUByte(msg, id);
 
-  uint8_t allowMovement;
-  nboUnpackUByte(msg, allowMovement);
-
-  uint8_t allowShooting;
-  nboUnpackUByte(msg, allowShooting);
+  uint8_t allow;
+  nboUnpackUByte(msg, allow);
 
   Player* tank = NULL;
   for (int i = 0; i < MAX_ROBOTS; i++) {
@@ -1113,10 +1110,10 @@ static void handleAllow ( void *msg, uint16_t /*len*/ )
       serverLink->sendDropFlag(localtank->getPosition());
   }
 
-  tank->setAllowMovement(allowMovement != 0);
-  tank->setAllowShooting(allowShooting != 0);
-  addMessage(tank, allowMovement ? "Movement allowed" : "Movement forbidden");
-  addMessage(tank, allowShooting ? "Shooting allowed" : "Shooting forbidden");
+  tank->setAllow(allow);
+  addMessage(tank, allow & AllowShoot ? "Shooting allowed" : "Shooting forbidden");
+  addMessage(tank, allow & (AllowMoveForward | AllowMoveBackward | AllowTurnLeft | AllowTurnRight) ? "Movement allowed" : "Movement restricted");
+  addMessage(tank, allow & AllowJump ? "Jumping allowed" : "Jumping forbidden");
 }
 
 static void handleKilledMessage ( void *msg, uint16_t /*len*/, bool, bool &checkScores )